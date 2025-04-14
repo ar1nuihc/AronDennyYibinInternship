3c0c2be39366612b512feadf487087670a6e6596
ar1nuihc <aron.chiu21@gmail.com>
2025-04-13 17:08:30 -0700

    about us page/route

--- app/routes/default.py
+++ app/routes/default.py
@@ -5,3 +5,8 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# About Us Page
+@app.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html')
